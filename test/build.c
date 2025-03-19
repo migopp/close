4ef9c4f96bd99f18fcdcb25db2c2044356fd8a94
Michael Goppert <goppert@utexas.edu>
2025-03-18 22:50:31 -0500

    Simple build script

---
+++ test/build.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
+#define RESET "\x1b[0m"
+
+int main(void) {
+  printf(YELLOW "NOTE: " RESET "Building `close` test files\n");
+
+  printf(BLUE "1: " RESET "Building `close` library files\n");
+  const char *cmd_build_close = "cd .. && make clean && make";
+  int rc = system(cmd_build_close);
+  if (rc < 0) {
+    fprintf(stderr,
+            RED "FAILURE: " RESET "Build of `close` failed, aborting...");
+    exit(-1);
+  }
+
+  printf(BLUE "2: " RESET "Building test infra and linking in `close` lib\n");
+  const char *cmd_build_test = "gcc -Werror -Wall -O0 -g -std=c99 -o "
+                               "./bin/test test.c ../target/close.o";
+  rc = system(cmd_build_test);
+  if (rc < 0) {
+    fprintf(stderr,
+            RED "FAILURE: " RESET "Build of `test` failed, aborting...");
+    exit(-1);
+  }
+
+  printf(BLUE "3: " RESET " Running `test`\n");
+  const char *cmd_run_test = "./bin/test";
+  rc = system(cmd_run_test);
+  if (rc < 0) {
+    fprintf(stderr, RED "FAILURE: " RESET "Run of `test` failed, aborting...");
+    exit(-1);
+  }
+
+  printf(GREEN "SUCCESS: " RESET "Build complete!\n");
+  return 0;
+}
