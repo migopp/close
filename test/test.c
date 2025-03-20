187c22cabb9b86cc815051178aacd8cb5203dbe5
Michael Goppert <goppert@utexas.edu>
2025-03-20 15:40:13 -0500

    More tests

--- test/test.c
+++ test/test.c
@@ -11,24 +11,61 @@
     exit(-1);                                                                  \
   }
 
+typedef struct thing_t {
+  unsigned a;
+  char b;
+  unsigned c;
+} thing_t;
+
 void fn_no_caps(void) { assert(true); }
 
 void fn_1_u32(uint32_t n) { assert(n == 1337); }
 
 void fn_1_u8(uint8_t n) { assert(n == 255); }
 
-void fn_2_u32(uint32_t n, uint32_t m) { assert(n == 420 && m == 6969); }
+void fn_2_u32(uint32_t n, uint32_t m) {
+  assert(n == 420);
+  assert(m == 6969);
+}
 
 void fn_6_u32(uint32_t n, uint32_t m, uint32_t o, uint32_t p, uint32_t q,
               uint32_t r) {
-  assert(n == 420 && m == 6969 && o == 1337 && p == 8008135 && q == 43110 &&
-         r == 42);
+  assert(n == 420);
+  assert(m == 6969);
+  assert(o == 1337);
+  assert(p == 8008135);
+  assert(q == 43110);
+  assert(r == 42);
 }
 
 void fn_7_u32(uint32_t n, uint32_t m, uint32_t o, uint32_t p, uint32_t q,
               uint32_t r, uint32_t s) {
-  assert(n == 420 && m == 6969 && o == 1337 && p == 8008135 && q == 43110 &&
-         r == 42 && s == 27);
+  assert(n == 420);
+  assert(m == 6969);
+  assert(o == 1337);
+  assert(p == 8008135);
+  assert(q == 43110);
+  assert(r == 42);
+  assert(s == 27);
+}
+
+void fn_1_thing(thing_t *t) {
+  assert(t->a == 1);
+  assert(t->b == 'a');
+  assert(t->c == 1);
+}
+
+void fn_6_u32_1_thing(uint32_t n, uint32_t m, uint32_t o, uint32_t p,
+                      uint32_t q, uint32_t r, thing_t *t) {
+  assert(n == 420);
+  assert(m == 6969);
+  assert(o == 1337);
+  assert(p == 8008135);
+  assert(q == 43110);
+  assert(r == 42);
+  assert(t->a == 20);
+  assert(t->b == '3');
+  assert(t->c == 27);
 }
 
 int main(void) {
@@ -99,6 +136,37 @@ int main(void) {
   c_r = init_capture(UINT32, &r_u32);
   capture_t c_s = init_capture(UINT32, &s_u32);
   c = init_closure((func_t)fn_7_u32, 7, c_n, c_m, c_o, c_p, c_q, c_r, c_s);
+  call_closure(c);
+  deinit_closure(c);
+
+  // `fn_1_thing`
+  printf("| testing fn w/ 1 thing_t capture\n");
+  thing_t n_t = {.a = 1, .b = 'a', .c = 1};
+  capture_t c_t = init_capture(VOIDPTR, &n_t);
+  c = init_closure((func_t)fn_1_thing, 1, c_t);
+  call_closure(c);
+  deinit_closure(c);
+
+  // `fn_6_u32_1_thing`
+  printf("| testing fn w/ 6 uint32_t captures and 1 thing_t capture\n");
+  n_u32 = 420;
+  m_u32 = 6969;
+  o_u32 = 1337;
+  p_u32 = 8008135;
+  q_u32 = 43110;
+  r_u32 = 42;
+  n_t.a = 20;
+  n_t.b = '3';
+  n_t.c = 27;
+  c_n = init_capture(UINT32, &n_u32);
+  c_m = init_capture(UINT32, &m_u32);
+  c_o = init_capture(UINT32, &o_u32);
+  c_p = init_capture(UINT32, &p_u32);
+  c_q = init_capture(UINT32, &q_u32);
+  c_r = init_capture(UINT32, &r_u32);
+  c_t = init_capture(VOIDPTR, &n_t);
+  c = init_closure((func_t)fn_6_u32_1_thing, 7, c_n, c_m, c_o, c_p, c_q, c_r,
+                   c_t);
   call_closure(c);
   deinit_closure(c);
 
